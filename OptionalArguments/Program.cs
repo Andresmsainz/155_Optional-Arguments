aee500e23fc8d7c18627d94b01faba15783f3f31
Andres Sainz <andresmsainz@gmail.com>
2021-10-19 23:04:50 -0400

    initial commit

---
+++ OptionalArguments/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OptionalArguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Optional Arguments!");
+            DisplayPlayerInformation(7, "C. Ronaldo", 60, "Portugal", "Real Madrid");
+            Console.WriteLine("------------------");
+            DisplayPlayerInformation(10, "L. Messi", 55);
+        }
+
+        public static void DisplayPlayerInformation(int num, string name, int goals, string country, string club)
+        {
+            Console.WriteLine("Hi From the first method");
+            Console.WriteLine("Number = " + num);
+            Console.WriteLine("Name = " + name);
+            Console.WriteLine("Goals = " + goals);
+            Console.WriteLine("Country = " + country);
+            Console.WriteLine("Club = " + club);
+        }
+
+        public static void DisplayPlayerInformation(int num, string name, int goals)
+        {
+            Console.WriteLine("Hi From the second method");
+            Console.WriteLine("Number = " + num);
+            Console.WriteLine("Name = " + name);
+            Console.WriteLine("Goals = " + goals);
+        }
+    }
+}
